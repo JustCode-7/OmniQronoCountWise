b162ae7e663934558f75c852d084ef93804eccdc
JustCode7 <harrypottz85@gmail.com>
2025-06-09 14:36:56 +0200

    test: play pause part 3

--- src/app/component/tabel-tennis-counter/tabel-tennis-counter.component.ts
+++ src/app/component/tabel-tennis-counter/tabel-tennis-counter.component.ts
@@ -93,37 +93,11 @@ export class TabelTennisCounterComponent implements OnInit, OnDestroy, AfterView
     }
 
     incrementPlayer1ScoreWithPlay() {
-        // Play the audio to trigger the MediaSession play event
-        if (this.dummyAudio && this.dummyAudio.nativeElement) {
-            const audioElement = this.dummyAudio.nativeElement;
-            audioElement.play().then(() => {
-                console.log('Audio played, MediaSession play event should be triggered');
-            }).catch(error => {
-                console.error('Error playing audio:', error);
-                // Fallback if play fails (e.g., user interaction required)
-                this.incrementPlayer1Score();
-            });
-        }
+        this.incrementPlayer1Score();
     }
 
     incrementPlayer2ScoreWithPause() {
-        if (this.dummyAudio && this.dummyAudio.nativeElement) {
-            const audioElement = this.dummyAudio.nativeElement;
-            if (!audioElement.paused) {
-                audioElement.pause();
-                console.log('Audio paused, MediaSession pause event should be triggered');
-            } else {
-                // If already paused, try to play first and then pause
-                audioElement.play().then(() => {
-                    audioElement.pause();
-                    console.log('Audio played and paused, MediaSession pause event should be triggered');
-                }).catch(error => {
-                    console.error('Error playing audio before pause:', error);
-                    // Fallback if play fails
-                    this.incrementPlayer2Score();
-                });
-            }
-        }
+        this.incrementPlayer2Score();
     }
 
     private setupMediaSession(): void {
